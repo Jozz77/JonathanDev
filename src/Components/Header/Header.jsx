b395704a148d500593ee1131ffb5cb540129c68b
Jozz77 <mmadujonathan@gmail.com>
2023-09-12 22:56:31 +0100

    Add changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,45 +1,152 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMobileSectionClick = () => {
+    setIsOpen(false); // Close the menu when a section link is clicked on mobile
+  };
+
   return (
-    <header className="hidden sm:flex bg-Primary z-10 justify-between px-[5%] md:px-[7%] lg:px-[10%] items-center fixed top-0 w-full py-3 xl:py-4 ">
-      <div className="text-[1.3rem] md:text-[1.5rem] lg:text-[1.7rem] xl:text-[2rem] font-Montserrat font-bold ">
-        Jo_Na_Than
+    <header className="bg-Primary z-10 fixed top-0 w-full py-3 xl:py-4">
+      <div className="sm:flex sm:justify-between md:items-center px-[5%] md:px-[7%] lg:px-[10%]">
+        <div className="flex items-center justify-between">
+          <div className="text-[1.2rem] sm:text-[1.3rem] md:text-[1.5rem] lg:text-[1.7rem] xl:text-[2rem] font-Montserrat font-bold ">
+            Jo_Na_Than
+          </div>
+          <div className="sm:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-white hover:text-gray cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-6 w-6 transform transition-transform ${
+                  isOpen ? "duration-300 rotate-90" : "duration-1000"
+                }`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+        <nav
+          className={`${
+            isOpen
+              ? "h-[100vh] transition-h duration-1000 ease-in-out"
+              : "h-[0] duration-300"
+          } sm:flex justify-between items-center w-[65%] md:w-[60%] xl:w-[50%] font-Montserrat font-bold text-[0.85rem] md:text-[0.9rem] lg:text-[1rem] overflow-hidden`}
+        >
+          {/* Desktop navigation links */}
+          <div className="hidden sm:flex justify-between items-center w-[100%] font-Montserrat font-bold text-[0.85rem] md:text-[0.9rem] lg:text-[1rem]">
+            <Link
+              to="about"
+              smooth={true}
+              duration={500}
+              offset={-80} // Desktop offset
+              className="block text-white hover:text-gray cursor-pointer"
+            >
+              About me
+            </Link>
+            <Link
+              to="portfolio"
+              smooth={true}
+              duration={500}
+              offset={-80} // Desktop offset
+              className="block text-white hover:text-gray cursor-pointer"
+            >
+              Portfolio
+            </Link>
+            <Link
+              to="blog"
+              smooth={true}
+              duration={500}
+              offset={-80} // Desktop offset
+              className="block text-white hover:text-gray cursor-pointer"
+            >
+              Blog
+            </Link>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              offset={-80} // Desktop offset
+              className="block text-white hover:text-gray cursor-pointer"
+            >
+              <button className=" border-solid border-black px-2 md:px-4 py-1 md:py-2 font-bold border-[3px] hover:text-gray hover:border-gray  ">
+                CONTACT ME
+              </button>
+            </Link>
+          </div>
+          {/* Mobile navigation links */}
+          <div className="block sm:hidden">
+            <Link
+              to="about"
+              smooth={true}
+              duration={500}
+              offset={-50}
+              className="block text-white hover:text-gray cursor-pointer"
+              onClick={handleMobileSectionClick}
+            >
+              About me
+            </Link>
+            <Link
+              to="portfolio"
+              smooth={true}
+              duration={500}
+              offset={-50}
+              className="block text-white hover:text-gray cursor-pointer"
+              onClick={handleMobileSectionClick}
+            >
+              Portfolio
+            </Link>
+            <Link
+              to="blog"
+              smooth={true}
+              duration={500}
+              offset={-50}
+              className="block text-white hover:text-gray cursor-pointer"
+              onClick={handleMobileSectionClick}
+            >
+              Blog
+            </Link>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              offset={-50}
+              className="block text-white hover:text-gray cursor-pointer"
+              onClick={handleMobileSectionClick}
+            >
+              <button className=" border-solid border-black px-2 md:px-4 py-1 md:py-2 font-bold border-[3px] hover:text-gray hover:border-gray  ">
+                CONTACT ME
+              </button>
+            </Link>
+          </div>
+        </nav>
       </div>
-      <nav className="flex justify-between items-center w-[65%] md:w-[60%] xl:w-[50%] font-Montserrat font-bold text-[0.85rem] md:text-[0.9rem] lg:text-[1rem] ">
-        <Link
-          to="about"
-          smooth={true}
-          duration={500}
-          offset={-80}
-          className="hover:text-gray cursor-pointer "
-        >
-          About me
-        </Link>
-        <Link
-          to="portfolio"
-          smooth={true}
-          duration={500}
-          offset={-80}
-          className="hover:text-gray cursor-pointer "
-        >
-          Portfolio
-        </Link>
-        <Link
-          to="blog"
-          smooth={true}
-          duration={500}
-          offset={-80}
-          className="hover:text-gray cursor-pointer "
-        >
-          Blog
-        </Link>
-        <Link to="contact" smooth={true} duration={500} offset={-80} className="cursor-pointer">
-          <button className=" border-solid border-black px-2 md:px-4 py-1 md:py-2 font-bold border-[3px] hover:text-gray hover:border-gray  ">
-            CONTACT ME
-          </button>
-        </Link>
-      </nav>
     </header>
   );
 }
--- src/Homepage/Components/Portfolio/Portfolio.jsx
+++ src/Homepage/Components/Portfolio/Portfolio.jsx
@@ -21,177 +21,187 @@ export default function Portfolio() {
     setActiveTab(tab);
   };
 
-
   return (
-    <div id="portfolio" className="bg-otherBlack text-whiteOther">
-
+    <div id="portfolio" className="bg-otherBlack  text-whiteOther">
       <section className="port-head bg-WhiteBoldText  py-4 ssm:py-6 lg:py-8 text-Secondary ">
-      <Heading text='MY PORTFOLIO' />
-
+        <Heading text="MY PORTFOLIO" />
       </section>
       <section className="w-full text-WhiteBoldText">
-        <div className="flex justify-center py-2 sm:py-4 mb-0 sm:mb-4 ">
+        <div className="flex flex-wrap justify-center py-2 sm:py-4 mb-0 sm:mb-4 ">
           <div
             onClick={() => handleTabClick("tab1")}
-            className={`cursor-pointer py-2 sm:py-4 px-2 ssm:px-6 sm:px-8 md:px-12 ${
+            className={`cursor-pointer py-2 sm:py-4 px-6 ssm:px-8 md:px-12 ${
               activeTab === "tab1"
                 ? "border-b-2 border-whiteOther transition duration-800 ease-in-out"
                 : "border-b-2 text-gray border-gray transition duration-800 ease-in-out"
-            } text-[0.7rem] ssm:text-[0.7rem] sm:text-[0.9rem] font-Montserrat font-semibold`}
+            } text-[0.8rem] sm:text-[0.9rem] font-Montserrat font-semibold`}
           >
             ALL
           </div>
           <div
             onClick={() => handleTabClick("tab2")}
-            className={`cursor-pointer py-2 sm:py-4 px-2 ssm:px-6 sm:px-8 md:px-12 ${
+            className={`cursor-pointer py-2 sm:py-4 px-6 ssm:px-8 md:px-12 ${
               activeTab === "tab2"
                 ? "border-b-2 border-whiteOther transition duration-800 ease-in-out"
                 : "border-b-2 text-gray border-gray transition duration-800 ease-in-out"
-            } text-[0.7rem] ssm:text-[0.7rem] sm:text-[0.9rem] font-Montserrat font-semibold`}
+            } text-[0.8rem] sm:text-[0.9rem] font-Montserrat font-semibold`}
           >
             PERSONAL PROJECTS
           </div>
           <div
             onClick={() => handleTabClick("tab3")}
-            className={`cursor-pointer py-2 sm:py-4 px-2 ssm:px-6 sm:px-8 md:px-12 ${
+            className={`cursor-pointer py-2 sm:py-4 px-6 ssm:px-8 md:px-12 ${
               activeTab === "tab3"
                 ? "border-b-2 border-whiteOther transition duration-800 ease-in-out"
                 : "border-b-2 text-gray border-gray transition duration-800 ease-in-out"
-            } text-[0.7rem] ssm:text-[0.7rem] sm:text-[0.9rem] font-Montserrat font-semibold`}
+            } text-[0.8rem] sm:text-[0.9rem] font-Montserrat font-semibold`}
           >
             GROUP PROJECTS
           </div>
         </div>
       </section>
-
-      {activeTab === "tab1" && (
-        <div className="flex flex-wrap">
-          {PortfolioData.map((portfolio, index) => {
-            const isHovered = index === hoveredIndex;
-            return (
-              <section
-                key={portfolio.id}
-                onMouseEnter={() => handleMouseEnter(index)}
-                onMouseLeave={handleMouseLeave}
-                className="relative mt-1 sm:mt-1 w-1/1 sm:w-1/2 lg:w-1/3 group"
-              >
-                <img src={portfolio.img} className="w-full" alt="" />
-                <div className={`absolute flex justify-center font-Montserrat  bg-Secondary bg-opacity-60 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
-                    !isHovered ? "opacity-100" : "opacity-0"
-                  }`}>
-                <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] md:text-[1.6rem] lg:text-[1.4rem] xl:text-[1.6rem] absolute bottom-[10%] font-bold tracking-[5px] mt-1">
-                    {portfolio.title}
-                  </h1>
-                </div>
-                <div
-                  className={`absolute flex flex-col items-center font-Montserrat justify-center bg-Secondary bg-opacity-80 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
-                    isHovered ? "opacity-100" : "opacity-0"
-                  }`}
+      <section className="port-body">
+        {activeTab === "tab1" && (
+          <div className="flex gap-y-1 flex-wrap">
+            {PortfolioData.map((portfolio, index) => {
+              const isHovered = index === hoveredIndex;
+              return (
+                <section
+                  key={portfolio.id}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className="relative w-1/1 sm:w-1/2 lg:w-1/3 group"
                 >
-                  <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] font-semibold">coded</p>
-                  <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] md:text-[1.6rem]  lg:text-[1.4rem] xl:text-[1.6rem] font-bold tracking-[5px] mt-1">
-                    {portfolio.title}
-                  </h1>
-                  <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] sm:px-[10%] px-[5%] lg:px-[15%] text-center font-medium mt-2 lg:mt-4">
-                    {portfolio.description}
-                  </p>
-                  <Link to={portfolio.link} target="_blank" className="">
-                  <Button text="LIVE SITE" />
-                  </Link>
-                </div>
-              </section>
-            );
-          })}
-        </div>
-      )}
+                  <img src={portfolio.img} className="w-full" alt="" />
+                  <div
+                    className={`absolute flex justify-center font-Montserrat  bg-Secondary bg-opacity-10 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
+                      !isHovered ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] md:text-[1.6rem] lg:text-[1.4rem] xl:text-[1.6rem] bg-otherBlack opacity-80 px-2 sm:px-4 bottom-[5%] absolute font-bold tracking-[5px] mt-1">
+                      {portfolio.title}
+                    </h1>
+                  </div>
+                  <div
+                    className={`absolute flex flex-col items-center font-Montserrat justify-center bg-Secondary bg-opacity-80 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
+                      isHovered ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] font-semibold">
+                      coded
+                    </p>
+                    <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] md:text-[1.6rem]  lg:text-[1.4rem] xl:text-[1.6rem] font-bold tracking-[5px] mt-1">
+                      {portfolio.title}
+                    </h1>
+                    <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] sm:px-[10%] px-[5%] lg:px-[15%] text-center font-medium mt-2 lg:mt-4">
+                      {portfolio.description}
+                    </p>
+                    <Link to={portfolio.link} target="_blank" className="">
+                      <Button text="LIVE SITE" />
+                    </Link>
+                  </div>
+                </section>
+              );
+            })}
+          </div>
+        )}
 
-  {activeTab === "tab2" && (
-    <div className="flex flex-wrap">
-      {PortfolioData.filter((portfolio) => portfolio.isPersonalProject).map((portfolio, index) => {
-        const isHovered = index === hoveredIndex;
-        return (
-          <section
-            key={portfolio.id}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-            className="relative mt-1 sm:mt-1 w-1/1 sm:w-1/2 lg:w-1/3 group"
-          >
-            <img src={portfolio.img} className="w-full" alt="" />
-            <div className={`absolute flex justify-center font-Montserrat  bg-Secondary bg-opacity-60 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
-                    !isHovered ? "opacity-100" : "opacity-0"
-                  }`}>
-                <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] absolute bottom-[10%] font-bold tracking-[5px] mt-1">
-                    {portfolio.title}
-                  </h1>
-                </div>
-            <div
-              className={`absolute flex flex-col items-center font-Montserrat justify-center bg-Secondary bg-opacity-80 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
-                isHovered ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] font-semibold">coded</p>
-              <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] font-bold tracking-[5px] mt-1">
-                {portfolio.title}
-              </h1>
-              <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] sm:px-[10%] px-[5%] lg:px-[15%] text-center font-medium mt-2 lg:mt-4">
-                {portfolio.description}
-              </p>
-              <Link to={portfolio.link} target="_blank" className="">
-              <Button text="LIVE SITE" />
-              </Link>
-            </div>
-          </section>
-        );
-      })}
-    </div>
-  )}
+        {activeTab === "tab2" && (
+          <div className="flex gap-y-1 flex-wrap">
+            {PortfolioData.filter(
+              (portfolio) => portfolio.isPersonalProject
+            ).map((portfolio, index) => {
+              const isHovered = index === hoveredIndex;
+              return (
+                <section
+                  key={portfolio.id}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className="relative w-1/1 sm:w-1/2 lg:w-1/3 group"
+                >
+                  <img src={portfolio.img} className="w-full" alt="" />
+                  <div
+                    className={`absolute flex justify-center font-Montserrat  bg-Secondary bg-opacity-10 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
+                      !isHovered ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] absolute bg-otherBlack opacity-80 px-2 sm:px-4 bottom-[5%] font-bold tracking-[5px] mt-1">
+                      {portfolio.title}
+                    </h1>
+                  </div>
+                  <div
+                    className={`absolute flex flex-col items-center font-Montserrat justify-center bg-Secondary bg-opacity-80 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
+                      isHovered ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] font-semibold">
+                      coded
+                    </p>
+                    <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] font-bold tracking-[5px] mt-1">
+                      {portfolio.title}
+                    </h1>
+                    <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] sm:px-[10%] px-[5%] lg:px-[15%] text-center font-medium mt-2 lg:mt-4">
+                      {portfolio.description}
+                    </p>
+                    <Link to={portfolio.link} target="_blank" className="">
+                      <Button text="LIVE SITE" />
+                    </Link>
+                  </div>
+                </section>
+              );
+            })}
+          </div>
+        )}
 
+        {activeTab === "tab3" && (
+          <div className="flex gap-y-1 flex-wrap">
+            {PortfolioData.filter(
+              (portfolio) => !portfolio.isPersonalProject
+            ).map((portfolio, index) => {
+              const isHovered = index === hoveredIndex;
+              return (
+                <section
+                  key={portfolio.id}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className="relative w-1/1 sm:w-1/2 lg:w-1/3 group"
+                >
+                  <img src={portfolio.img} className="w-full" alt="" />
+                  <div
+                    className={`absolute flex justify-center font-Montserrat  bg-Secondary bg-opacity-10 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
+                      !isHovered ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] absolute bg-otherBlack opacity-80 px-2 sm:px-4 bottom-[5%] font-bold tracking-[5px] mt-1">
+                      {portfolio.title}
+                    </h1>
+                  </div>
+                  <div
+                    className={`absolute flex flex-col items-center font-Montserrat justify-center bg-Secondary bg-opacity-80 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
+                      isHovered ? "opacity-100" : "opacity-0"
+                    }`}
+                  >
+                    <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] font-semibold">
+                      coded
+                    </p>
+                    <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] font-bold tracking-[5px] mt-1">
+                      {portfolio.title}
+                    </h1>
+                    <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] sm:px-[10%] px-[5%] lg:px-[15%] text-center font-medium mt-2 lg:mt-4">
+                      {portfolio.description}
+                    </p>
+                    <Link to={portfolio.link} target="_blank" className="">
+                      <Button text="LIVE SITE" />
+                    </Link>
+                  </div>
+                </section>
+              );
+            })}
+          </div>
+        )}
+      </section>
 
-{activeTab === "tab3" && (
-    <div className="flex flex-wrap">
-      {PortfolioData.filter((portfolio) => !portfolio.isPersonalProject).map((portfolio, index) => {
-        const isHovered = index === hoveredIndex;
-        return (
-          <section
-            key={portfolio.id}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-            className="relative mt-1 sm:mt-1 w-1/1 sm:w-1/2 lg:w-1/3 group"
-          >
-            <img src={portfolio.img} className="w-full" alt="" />
-            <div className={`absolute flex justify-center font-Montserrat  bg-Secondary bg-opacity-60 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
-                    !isHovered ? "opacity-100" : "opacity-0"
-                  }`}>
-                <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] absolute bottom-[10%] font-bold tracking-[5px] mt-1">
-                    {portfolio.title}
-                  </h1>
-                </div>
-            <div
-              className={`absolute flex flex-col items-center font-Montserrat justify-center bg-Secondary bg-opacity-80 bottom-0 right-0 top-0 left-0 bg-white p-4 w-full shadow-md transition-opacity duration-1000 ${
-                isHovered ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] font-semibold">coded</p>
-              <h1 className="text-[1.2rem] ssm:text-[1.6rem] sm:text-[1.1rem] lg:text-[1.4rem] xl:text-[1.6rem] font-bold tracking-[5px] mt-1">
-                {portfolio.title}
-              </h1>
-              <p className="text-[0.8rem] ssm:text-[0.8rem] sm:text-[0.7rem] sm:px-[10%] px-[5%] lg:px-[15%] text-center font-medium mt-2 lg:mt-4">
-                {portfolio.description}
-              </p>
-              <Link to={portfolio.link} target="_blank" className="">
-                <Button text="LIVE SITE" />
-                  
-
-              </Link>
-            </div>
-          </section>
-        );
-      })}
-    </div>
-  )}
-
-
-      <section className="text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] font-Montserrat font-semibold text-center py-4">
+      <section className="text-[0.8rem] sm:text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] font-Montserrat font-semibold text-center py-2 sm:py-4">
         <p>And many more to come!</p>
       </section>
     </div>
