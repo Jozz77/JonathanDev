953a534391898412b64ad71d3c5fbfed9890d61b
Jozz77 <mmadujonathan@gmail.com>
2023-09-18 11:10:17 +0100

    Done

--- src/Homepage/Components/Contact.jsx
+++ src/Homepage/Components/Contact.jsx
@@ -1,10 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../../Components/Heading";
 import Seperator from "../../Components/Seperator";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
-
 
 export default function Contact() {
   useEffect(() => {
@@ -20,6 +19,27 @@ export default function Contact() {
   useEffect(() => {
     AOS.refresh();
   }, []);
+
+  const [isLoading, setIsLoading] = useState(false); // State variable to track loading
+  const [isSubmitted, setIsSubmitted] = useState(false); // State variable to submit form
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Set isLoading to true when the form is submitted
+    setIsLoading(true);
+
+    // Simulate an API call or form submission delay (you can replace this with your actual submission logic)
+    setTimeout(() => {
+      // After the submission is complete, set isLoading back to false
+      setIsLoading(false);
+      setIsSubmitted(true);
+
+      // Reset the form or perform any other necessary actions
+      console.log("Form submitted!");
+    }, 2000); // Simulated 2-second delay
+  };
+
   return (
     <div
       data-aos="zoom-in-right"
@@ -44,6 +64,7 @@ export default function Contact() {
       </section>
 
       <form
+        onSubmit={handleSubmit}
         action=""
         className=" font-Montserrat bg-none text-Secondary py-8 lg:py-10 xl:py-12 w-[70%] sm:w-[60%] md:w-[50%] lg:w-[40%] mx-auto "
       >
@@ -72,21 +93,56 @@ export default function Contact() {
         />
 
         <textarea
-          className="w-full mt-8 xl:mt-10 text-[0.8rem] sm:text-[0.85rem] lg:text-[0.9rem] xt-[0.75rem] eholder:ssm:text-[0.8rem] placeholder:sm:text-[0.85rem] placeholder:lg:text-[0.9rem] focus:border-gray placeholder:text-gray px-[2%] pb-16 bg-[transparent] border-solid border-b-[4px] border-l-[4px] border-Secondary outline-none "
+          className="w-full mt-8 xl:mt-10 text-[0.8rem] sm:text-[0.85rem] lg:text-[0.9rem] xt-[0.75rem] eholder:ssm:text-[0.8rem] placeholder:sm:text-[0.85rem] placeholder:lg:text-[0.9rem] focus:border-gray placeholder:text-gray px-[2%] pb-16 bg-[transparent] border-solid border-b-[4px] border-l-[4px] border-Secondary resize-none outline-none "
           type="text"
           name="name"
           placeholder="YOUR MESSAGE* "
           id=""
         />
 
-        <div className="flex justify-center mt-3 sm:mt-4 xl:mt-6  ">
-          <button className=" border-solid border-black px-6 md:px-8 py-1 md:py-2 text-[0.8rem] ssm:text-[0.9rem] sm:text-base font-bold border-[3px] hover:text-gray hover:border-gray  ">
-            SEND
-          </button>
+        <div className="flex justify-center mt-3 sm:mt-4 xl:mt-6">
+          {isLoading ? (
+            <button
+              disabled=""
+              type="button"
+              className="border-solid border-black px-6 md:px-8 py-1 md:py-2 text-[0.8rem] ssm:text-[0.9rem] sm:text-base font-bold border-[3px] cursor-not-allowed"
+            >
+              <svg
+                aria-hidden="true"
+                role="status"
+                className="inline mr-2 border-solid border-black border-[1px] rounded-full w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                ></path>
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#1C64F2"
+                ></path>
+              </svg>
+              Loading...
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="border-solid border-black px-6 md:px-8 py-1 md:py-2 text-[0.8rem] ssm:text-[0.9rem] sm:text-base font-bold border-[3px] hover:text-gray hover:border-gray"
+            >
+              SEND
+            </button>
+          )}
+        </div>
+        <div className=" flex justify-center font-bold mt-4 ">
+          {isSubmitted && (
+            <div>
+              Thanks for submitting!!!
+            </div>
+          )}
         </div>
       </form>
     </div>
-    
   );
-
 }
