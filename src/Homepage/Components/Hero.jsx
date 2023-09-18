4b24d5f2f6d3d0d3c376fa82144bf92d1e539149
Jozz77 <mmadujonathan@gmail.com>
2023-09-18 11:13:21 +0100

    done

--- src/Homepage/Components/Hero.jsx
+++ src/Homepage/Components/Hero.jsx
@@ -16,7 +16,6 @@ export default function Hero() {
       // initClassName: 'contact', // class applied after initialization
       animatedClassName: "hero", // class applied on animation
       once: false, // whether animation should happen only once - while scrolling down
-      delay: 200,
     });
   }, []);
 
