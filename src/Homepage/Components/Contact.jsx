fb5ba341a9feb1a840b7140a1c17fc0b8c929b8f
Jozz77 <mmadujonathan@gmail.com>
2023-10-07 02:39:23 +0100

    new

--- src/Homepage/Components/Contact.jsx
+++ src/Homepage/Components/Contact.jsx
@@ -66,7 +66,7 @@ export default function Contact() {
       <form
         onSubmit={handleSubmit}
         action=""
-        className=" font-Montserrat bg-none text-Secondary py-8 lg:py-10 xl:py-12 w-[70%] sm:w-[60%] md:w-[50%] lg:w-[40%] mx-auto "
+        className=" font-Montserrat bg-none text-Secondary py-8 lg:py-10 xl:py-12 w-[80%] sm:w-[60%] md:w-[50%] lg:w-[40%] mx-auto "
       >
         <input
           className="w-full mt-4 text-[0.8rem] sm:text-[0.85rem] lg:text-[0.9rem]  placeholder:text-[0.8rem] placeholder:sm:text-[0.85rem] placeholder:lg:text-[0.9rem] focus:border-gray placeholder:text-gray px-[4%] ssm:px-[2%] py-2 bg-[transparent] border-solid border-b-[4px] border-l-[4px] border-Secondary outline-none "
@@ -105,26 +105,26 @@ export default function Contact() {
             <button
               disabled=""
               type="button"
-              className="border-solid border-black px-6 md:px-8 py-1 md:py-2 text-[0.8rem] ssm:text-[0.9rem] sm:text-base font-bold border-[3px] cursor-not-allowed"
+              className="border-solid border-[1C64F2] px-6 md:px-8 py-1 md:py-2 text-[0.8rem] ssm:text-[0.9rem] sm:text-base font-bold border-[3px] cursor-not-allowed"
             >
               <svg
                 aria-hidden="true"
                 role="status"
-                className="inline mr-2 border-solid border-black border-[1px] rounded-full w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+                className="inline mr-2 border-solid border-[1C64F2] border-[1px] rounded-full w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
+                  fill="#1C64F2"
                 ></path>
                 <path
                   d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                   fill="#1C64F2"
                 ></path>
               </svg>
-              Loading...
+              SENDING...
             </button>
           ) : (
             <button
